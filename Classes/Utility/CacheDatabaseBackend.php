d5aa67a9e3093f94cdc0ed1e8e5879274793b4a0
Johannes C. Schulz <johannes.schulz@kvt.de>
2018-10-16 11:31:14 +0200

    removing Class

--- Classes/Utility/CacheDatabaseBackend.php
+++ Classes/Utility/CacheDatabaseBackend.php
@@ -27,7 +27,6 @@ namespace Tx\Webkitpdf\Utility;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Core\Database\ConnectionPool;
 use TYPO3\CMS\Core\Cache\Backend\AbstractBackend;
-use TYPO3\CMS\Core\Cache\Backend\PhpCapableBackendInterface;
 use TYPO3\CMS\Core\Cache;
 
 /**
